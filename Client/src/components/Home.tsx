ff25c4835d5a966bda98b16938feeef87e29c0f6
tmock <tabea.mock@upc.ch>
2022-12-23 16:21:11 +0100

    continue with homepage

--- Client/src/components/Home.tsx
+++ Client/src/components/Home.tsx
@@ -5,42 +5,54 @@ import donutpicture from '../img/img.png';
 
 
 function Home() {
+
     return (
         <div className={"home"}>
             <h1>Donut auswählen</h1>
             <div className={"donuts"}>
-                <Card variant={"elevation"} className={'card'}>
-                    <img src={donutpicture} alt="Donut"/>
-                    <div className={"cardConatiner"}>
-                        <h2>Donut Natur</h2>
-                        <input type="radio" name="donutRadioButton" />
-                    </div>
-                </Card>
-                <Card variant={"elevation"} className={'card'}>
-                    <img src={donutpicture} alt="Donut"/>
-                    <div className={"cardConatiner"}>
-                        <h2>Donut Vegan</h2>
-                        <input type="radio" name="donutRadioButton"/>
+                    <Card variant={"elevation"} className={'card'}>
+                        <img src={donutpicture} alt="Donut"/>
+                        <div className={"cardConatiner"}>
+                            <h2>Donut Natur</h2>
+                        </div>
+                    </Card>
+                    <Card variant={"elevation"} className={'card'}>
+                        <img src={donutpicture} alt="Donut"/>
+                        <div className={"cardConatiner"}>
+                            <h2>Donut Vegan</h2>
 
-                    </div>
-                </Card>
-                <Card variant={"elevation"} className={'card'}>
-                    <img src={donutpicture} alt="Donut"/>
-                    <div className={"cardConatiner"}>
-                        <h2>Donut Gefüllt</h2>
-                        <input type="radio" name="donutRadioButton"/>
-
-                    </div>
-                </Card>
+                        </div>
+                    </Card>
+                    <Card variant={"elevation"} className={'card'}>
+                        <img src={donutpicture} alt="Donut"/>
+                        <div className={"cardConatiner"}>
+                            <h2>Donut Gefüllt</h2>
+                        </div>
+                    </Card>
             </div>
 
             <h1>Glasur auswählen</h1>
             <div className={"glasur"}>
                 <Card variant={"elevation"} className={'card'}>
                     <p>Schokolade</p>
+
                 </Card>
                 <Card variant={"elevation"} className={'card'}>
                     <p>Weisse Schokolade</p>
+                </Card>
+            </div>
+
+            <h1>Toppings</h1>
+            <div className={"toppings"}>
+                <Card variant={"elevation"} className={'card'}>
+                    <p>Erdbeeren</p>
+                </Card>
+                <Card variant={"elevation"} className={'card'}>
+                    <p>Blaubeeren</p>
+                </Card>
+                <Card variant={"elevation"} className={'card'}>
+                    <p>Kinderschokolade</p>
+
                 </Card>
             </div>
         </div>
